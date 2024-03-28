23fea9175a944f67bb08ecf26bd0305253e88017
Nicolas DUBIEN <github@dubien.org>
2024-03-28 08:00:50 +0000

    rework example for proposal

--- main.ts
+++ main.ts
@@ -6,6 +6,9 @@ import { identifier as identifierMJS } from "./mod-mjs.mjs";
 // is not compatible with one coming from MJS one. But in practice, identifiers are just raw objects
 // with depth in the context of fast-check. As such there is no problem with taking one from CJS world
 // and use it in MJS one.
-function withId(identifier: fcMJS.DepthIdentifier) {}
-withId(identifierCJS); // used not to compile in fast-check 3.17.0
-withId(identifierMJS);
+function getDepth(identifier: fcMJS.DepthIdentifier) {
+  return fcMJS.getDepthContextFor(identifier);
+}
+getDepth(identifierCJS); // used not to compile in fast-check 3.17.0
+getDepth(identifierMJS);
+getDepth({}); // compiled with proposals/fast-check-pr4842-6a46d52.tgz
